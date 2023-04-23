e361fb35599719f37ee036b2f8e8c6a953c5a2b4
sepehrzoli <119758524+sepehrzoli@users.noreply.github.com>
2023-04-23 10:05:50 +0330

    login menu changed

--- src/main/java/Model/Regex/LoginRegexes.java
+++ src/main/java/Model/Regex/LoginRegexes.java
@@ -1,5 +1,7 @@
 package Model.Regex;
 
+import View.SecurityQuestion;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,11 +9,12 @@ public enum LoginRegexes {
     REGISTER("user\\s+create\\s+.+"),
     NICKNAME("\\-n\\s+((?<nickname>\\S+)|(\"(?<nickname>.+)\"))"),
     USERNAME("\\-u\\s+((?<username>\\S+)|(\"(?<username>.+)\"))"),
-    PASSWORD("\\-p\\s+((?<password>\\S+)|(\"(?<password>.+)\"))\\s+((?<repeated>\\S+)|(\"(?<repeated>.+)\"))"),
+    PASSWORD("\\-p\\s+((?<password>\\S+)|(\"(?<password>.+)\"))"),
     PASSWORD_CONFIRMATION("\\-p\\s+((?<password>\\S+)|(\"(?<password>.+)\"))\\s+((?<repeated>\\S+)|(\"(?<repeated>.+)\"))"),
     EMAIL("\\-email\\s+(?<email>\\S+)"),
     SLOGAN("\\-s\\s+((?<slogan>\\S+)|(\"(?<slogan>.+)\"))"),
-    IS_A_FIELD_EMPTY("\\-\\S+\\s+\\-");
+    IS_A_FIELD_EMPTY("\\-\\S+\\s+\\-"),
+    PICK_QUESTION("question\\s+pick\\s+-q\\s+(?<number>\\d)\\s+-a\\s+((?<answer>\\S+)|(\"(?<answer>.+)\"))\\s+-c\\s+((?<confirm>\\S+)|(\"(?<confirm>.+)\"))");
 
     private String regex;
     private LoginRegexes(String regex){
